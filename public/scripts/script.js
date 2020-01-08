e5204546ff1ea68e28c5cc9dde1723b5a3d2d3f0
sahiltgi <abhi28005@gmail.com>
2020-01-08 20:25:52 +0530

    modal fixes

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -120,24 +120,22 @@ function myFunction(xml) {
   document.getElementById("result").innerHTML = table;
 }
 
-function openModal() {
-  var modal = document.getElementById("Book-Rating-modal");
+var modal = document.getElementById("Book-Rating-modal");
 
-  var btn = document.getElementById("Rate-Book");
+var btn = document.getElementById("Rate-Book");
 
-  var close = document.getElementsByClassName("close")[0];
+var close = document.getElementsByClassName("close")[0];
 
-  btn.onclick = function() {
-    modal.style.display = "block";
-  };
+btn.onclick = function() {
+  modal.style.display = "block";
+};
 
-  close.onclick = function() {
+close.onclick = function() {
+  modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+  if (event.target == modal) {
     modal.style.display = "none";
-  };
-
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
-}
+  }
+};
