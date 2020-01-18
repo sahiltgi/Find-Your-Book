0ed195ac7e798f2786c349870236588d41129ae4
sahiltgi <abhi28005@gmail.com>
2020-01-18 17:55:35 +0530

    temp

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -92,10 +92,8 @@ function find() {
 function myFunction(xml) {
   var i;
   var xmlDoc = xml.responseXML;
-  const bookname = x[i].getElementsByTagName("title")[0].childNodes[0]
-    .nodeValue;
-  const authorname = x[i].getElementsByTagName("name")[0].childNodes[0]
-    .nodeValue;
+  let bookname = x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+  let authorname = x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
   var table =
     "<tr><th>Title</th><th>Author</th><th>Cover Page</th><th>Ratings</th></tr>";
   var x = xmlDoc.getElementsByTagName("best_book");
